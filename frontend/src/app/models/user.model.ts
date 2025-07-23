73387af3c34651f65807b7ac1661e629e60f829a
rojan <coderojan001@gmail.com>
2025-07-23 22:25:58 +0545

    Added login and register flow

--- frontend/src/app/models/user.model.ts
+++ frontend/src/app/models/user.model.ts
@@ -1,6 +1,18 @@
 
-export class User {
+export interface User {
   id?: number;
   username?: string;
   password?: string;
 }
+
+export interface LoginResponse {
+  token: string;
+  
+}
+
+export interface RegisterResponse {
+  message: string;
+
+}
+
+
---
+++ frontend/src/app/routing-constants.ts
@@ -0,0 +1,6 @@
+
+export class RoutingConstants {
+  static readonly AUTH = '/auth';
+  static readonly LOGIN = '/login';
+  static readonly REGISTER = '/register';
+}
