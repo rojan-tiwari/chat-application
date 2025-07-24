6af20b074755365e66c5a195e309e2ee2cd80a52
rojan <coderojan001@gmail.com>
2025-07-24 22:33:59 +0545

    Added frontend release CI

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,56 +1,27 @@
 
-import { Component, OnInit, Optional } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
-import { Router } from '@angular/router';
-
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [FormsModule,CommonModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent implements OnInit {
-
   
   loginRequest = {
     username: '',
     password: ''
   };
-
-  errorMessage: string = ''; 
-  isLoading = false;  
-  showPassword = false;
-
   
-  constructor(private authService: AuthService, private router: Router) {
-}
-
+  constructor(private authService: AuthService) { }
   
   ngOnInit(): void {
   }
-
-  togglePassword() {
-    this.showPassword = !this.showPassword;
-  }
-
   
-   login() {
-    this.isLoading = true; 
-    this.authService.login(this.loginRequest).subscribe({
-      next: (response) => {
-        console.log(response);
-        this.isLoading = false; 
-        this.router.navigate(['/register']); 
-      },
-      error: (error) => {
-        console.error(error);
-        this.isLoading = false; 
-      }
+  login() {
+    this.authService.login(this.loginRequest).subscribe(response => {
+      console.log(response);
     });
   }
 }
