800a239270744d78f4d88b04590b3e2e007ddc14
rojan <coderojan001@gmail.com>
2025-08-08 16:33:53 +0545

    added chat functionality with obsever pattern

--- backend/src/main/java/com/chatapp/configuration/WebSocketConfig.java
+++ backend/src/main/java/com/chatapp/configuration/WebSocketConfig.java
@@ -1,7 +1,7 @@
 package com.chatapp.configuration;
 
 import com.chatapp.observer.ChatRoom;
-import com.chatapp.observer.ChatWebSocketHandler;
+import com.chatapp.repository.ChatMessageRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.*;
@@ -10,17 +10,21 @@ import org.springframework.web.socket.config.annotation.*;
 @EnableWebSocket
 public class WebSocketConfig implements WebSocketConfigurer {
 
-    private final ChatRoom chatRoom = new ChatRoom(); // shared across handlers
+    private final ChatMessageRepository chatMessageRepository;
+
+    public WebSocketConfig(ChatMessageRepository chatMessageRepository) {
+        this.chatMessageRepository = chatMessageRepository;
+    }
+
+    // singleton shared room
+    @Bean
+    public ChatRoom chatRoom() {
+        return new ChatRoom();
+    }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(chatWebSocketHandler(), "/chat")
-            .setAllowedOrigins("*");
-    }
-
-    @Bean
-    public ChatWebSocketHandler chatWebSocketHandler() {
-        return new ChatWebSocketHandler(chatRoom);
+        registry.addHandler(new ChatWebSocketHandlerFactory(chatRoom(), chatMessageRepository), "/chat")
+            .setAllowedOrigins("http://localhost:4200");
     }
 }
-
--- backend/src/main/java/com/chatapp/controller/AuthController.java
+++ backend/src/main/java/com/chatapp/controller/AuthController.java
@@ -1,14 +1,16 @@
 package com.chatapp.controller;
 
+import com.chatapp.exception.UsernameAlreadyExistsException;
 import com.chatapp.model.entity.User;
 import com.chatapp.model.entity.record.LoginRequest;
+import com.chatapp.service.UserService;
+import com.chatapp.util.BaseResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import com.chatapp.service.UserService;
 
 import java.util.Optional;
 
@@ -20,17 +22,28 @@ public class AuthController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<User> registerUser(@RequestBody User user) {
-        return ResponseEntity.ok(userService.registerUser(user));
+    public ResponseEntity<BaseResponse<String>> registerUser(@RequestBody User user) {
+        try {
+            userService.registerUser(user);
+            return ResponseEntity.ok(BaseResponse.success("User Registered Successfully",null));
+        } catch (UsernameAlreadyExistsException e) {
+            return ResponseEntity.badRequest().body(BaseResponse.error(400, e.getMessage()));
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(BaseResponse.error(400, "An unexpected error occurred"));
+        }
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest loginRequest) {
-        Optional<User> user = userService.findByUsername(loginRequest.username());
-        if (user.isPresent() && user.get().getPassword().equals(loginRequest.password())) {
-            return ResponseEntity.ok("Login successful");
-        } else {
-            return ResponseEntity.badRequest().body("Invalid username or password");
+    public ResponseEntity<BaseResponse<Object>> login(@RequestBody LoginRequest loginRequest) {
+        try {
+            Optional<User> user = userService.findByUsername(loginRequest.username(), loginRequest.password());
+            if (user.isPresent()) {
+                return ResponseEntity.ok(BaseResponse.success(user,"Login was successful"));
+            } else {
+                return ResponseEntity.badRequest().body(BaseResponse.error(400, "Invalid username or password"));
+            }
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(BaseResponse.error(400, e.getMessage()));
         }
     }
 }
